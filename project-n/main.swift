9d0aa4b15dacd71471eb454fc6684e4a133bf55d
Miraion <j.schwartz564@icloud.com>
2017-10-15 16:41:13 -0700

    Initial Commit

---
+++ project-n/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  project-n
+//
+//  Created by Jeremy S on 2017-10-15.
+//  Copyright © 2017 Jeremy S. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
